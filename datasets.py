5157f9379e0cfb7d727103e4000a8795a22d13fb
pkurz3nd <pk@localyzer.de>
2023-01-02 12:43:02 +0100

    save

---
+++ datasets.py
@@ -0,0 +1,324 @@
+
+from transformers import CLIPTextModel, CLIPTokenizer
+from transformers import BatchEncoding
+import random
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, DDIMScheduler, DPMSolverMultistepScheduler, LMSDiscreteScheduler, EulerDiscreteScheduler, DDPMScheduler, UNet2DConditionModel, AutoencoderKL
+
+
+from torch.utils.data import Dataset
+
+import PIL
+
+from PIL import Image
+
+import itertools
+import math
+import os
+import random
+from pathlib import Path
+
+from torchvision import transforms
+
+import json 
+
+
+
+
+# dreambooth:
+    # single prompt: photo of xyz house
+    # pick random from list of predefined prompts
+    # image caption pairs
+    # train multiple concepts jointly
+
+# textual inversion
+    # pick random from list of predefined prompts
+    # image caption pairs
+    # train multiple concepts jointly
+
+# finetuning:
+    # image caption pairs
+
+
+
+# dreambooth collate_fn output
+    # batch = {
+    #     "input_ids": input_ids,
+    #     "pixel_values": pixel_values,
+    # }
+    #     batch = {
+    #         "input_ids": input_ids,
+    #         "pixel_values": pixel_values,
+        # }
+
+
+
+
+# precedence:
+# concept_list keys:
+    # instance_prompt
+    # instance_data_dir
+
+
+    # class_prompt
+    # class_data_dir
+
+
+    # promptsfile     each line: {"file_name": "0001.png", "text": "This is a golden retriever playing with a ball"}
+    # metadata      each line: {"file_name": "0001.png", "text": "This is a golden retriever playing with a ball"}
+
+
+
+# if promtsfile is None, use instance_prompt
+# if promtsfile and instance_prompt is None, use ramdom prompt from templates
+# if token is set, must be contained in instance_prompt, if instance_prompt is not set, should be contained in promptsfile (maybe if not in all, it has regularization effect)
+# for normal finetuning, only set promptsfile and instance_data_dir, everything else set to None
+concepts_list = [
+            {
+                "token" : None, #instance_prompt must contain token (if both set), if not set, instance_prompt can contain eg rare tokens like xyz, worth trying if some prompts in promptsfile do not contain the token if it helps for preserving prior and regularization
+                "initializer_token" : None, # only needed when adding "token" to embedding
+                "promptsfile" : None, # precedence 1
+                "instance_prompt": None, # precedence 2
+                "class_prompt": None,
+                "instance_data_dir":None,
+                "class_data_dir": None,
+                "type" : None, # one of ['style', 'object']; must be set if promptsfile and instance_prompt are not set
+                
+            }
+        ]
+
+imagenet_templates_small = [
+    "a photo of a {}",
+    "a rendering of a {}",
+    "a cropped photo of the {}",
+    "the photo of a {}",
+    "a photo of a clean {}",
+    "a photo of a dirty {}",
+    "a dark photo of the {}",
+    "a photo of my {}",
+    "a photo of the cool {}",
+    "a close-up photo of a {}",
+    "a bright photo of the {}",
+    "a cropped photo of a {}",
+    "a photo of the {}",
+    "a good photo of the {}",
+    "a photo of one {}",
+    "a close-up photo of the {}",
+    "a rendition of the {}",
+    "a photo of the clean {}",
+    "a rendition of a {}",
+    "a photo of a nice {}",
+    "a good photo of a {}",
+    "a photo of the nice {}",
+    "a photo of the small {}",
+    "a photo of the weird {}",
+    "a photo of the large {}",
+    "a photo of a cool {}",
+    "a photo of a small {}",
+]
+imagenet_style_templates_small = [
+    "a photo in the style of {}",
+    "a rendering in the style of {}",
+    "a cropped photo in the style of {}",
+    "the photo in the style of {}",
+    "a clean photo in the style of {}",
+    "a dirty photo in the style of {}",
+    "a dark photo in the style of {}",
+    "a picture in the style of {}",
+    "a cool photo in the style of {}",
+    "a close-up photo in the style of {}",
+    "a bright photo in the style of {}",
+    "a cropped photo in the style of {}",
+    "a good photo in the style of {}",
+    "a close-up photo in the style of {}",
+    "a rendition in the style of {}",
+    "a nice photo in the style of {}",
+    "a small picture in the style of {}",
+    "a weird picture in the style of {}",
+    "a large picture in the style of {}",
+]
+
+
+def get_promptsfile(file_path):
+    try:
+        with open(file_path, "r") as f:
+            data = [json.loads(line) for line in f]
+    except Exception as e:
+        print(e)
+        data = []
+    return data
+
+# entry = {"file_name": filename, "text": prompt},
+
+# imagenet_style_templates_small = [
+#     "a painting in the style of {}",
+#     "a rendering in the style of {}",
+#     "a cropped painting in the style of {}",
+#     "the painting in the style of {}",
+#     "a clean painting in the style of {}",
+#     "a dirty painting in the style of {}",
+#     "a dark painting in the style of {}",
+#     "a picture in the style of {}",
+#     "a cool painting in the style of {}",
+#     "a close-up painting in the style of {}",
+#     "a bright painting in the style of {}",
+#     "a cropped painting in the style of {}",
+#     "a good painting in the style of {}",
+#     "a close-up painting in the style of {}",
+#     "a rendition in the style of {}",
+#     "a nice painting in the style of {}",
+#     "a small painting in the style of {}",
+#     "a weird painting in the style of {}",
+#     "a large painting in the style of {}",
+# ]
+
+
+class StableDiffusionDataset(Dataset):
+    """
+    A dataset to prepare the instance and class images with the prompts for fine-tuning the model.
+    It pre-processes the images and the tokenizes prompts.
+    """
+
+    def __init__(
+        self,
+        concepts_list,
+        tokenizer,
+        with_prior_preservation=True,
+        size=512,
+        center_crop=False,
+        num_class_images=None,
+        pad_tokens=False,
+        hflip=False,
+        vector_shuffle=False
+    ):
+        self.size = size
+        self.center_crop = center_crop
+        self.tokenizer = tokenizer
+        self.with_prior_preservation = with_prior_preservation
+        self.pad_tokens = pad_tokens
+
+        self.vector_shuffle = vector_shuffle
+
+        self.instance_images_path = []
+        self.class_images_path = []
+
+        if isinstance(concepts_list, str):
+            try:
+                with open(concepts_list, "r") as f:
+                    concepts_list = json.load(f)
+            except Exception as e:
+                raise ValueError(f"concept_list is a string and cannot be opened. if concept_list is a string, there must exist a file with this filepath.")
+                  
+                
+
+        for concept in concepts_list:
+            # if promtsfile is set, use promtsfile
+            # if promtsfile is None, use instance_prompt
+            # if promtsfile and instance_prompt are None, use ramdom prompt from templates
+
+            if concept.get("promptsfile") is not None:
+                promptsfile = get_promptsfile(concept["promptsfile"])
+                inst_img_path = [(x['file_name'], x['text']) for x in promptsfile if os.path.exists(concept["instance_data_dir"].rstrip('/') + '/' +x['file_name'])]
+                self.instance_images_path.extend(inst_img_path)
+
+            elif concept.get("instance_prompt") is not None:
+                if concept.get("token"): 
+                    assert concept["token"] in concept["instance_prompt"], 'if "token" and "instance_prompt" are set, "instance_prompt" must contain "token".'
+
+                inst_img_path = [(x, concept["instance_prompt"]) for x in Path(concept["instance_data_dir"]).iterdir() if x.is_file()] # [('./abc.png', 'photo of xyz house'), ...]
+                self.instance_images_path.extend(inst_img_path)
+
+            else:
+                if concept.get('type') is None:
+                    raise ValueError('if promptsfile and instance_prompt are not set in concept_list, "type" must be specified as one of ["style", "object"].')
+                templates = imagenet_style_templates_small if concept['type'] == 'style' else imagenet_templates_small
+                sample_prompt_from_template = lambda: random.choice(templates).format(concept["token"])
+                inst_img_path = [(x, sample_prompt_from_template()) for x in Path(concept["instance_data_dir"]).iterdir() if x.is_file()]
+                self.instance_images_path.extend(inst_img_path)
+
+            if num_class_images and with_prior_preservation:
+                class_img_path = [(x, concept["class_prompt"]) for x in Path(concept["class_data_dir"]).iterdir() if x.is_file()] # [('./abc.png', 'photo of house'), ...]
+                self.class_images_path.extend(class_img_path[:num_class_images])
+
+        random.shuffle(self.instance_images_path)
+
+        self.num_instance_images = len(self.instance_images_path)
+        self._length = self.num_instance_images
+        if num_class_images:
+            self.num_class_images = len(self.class_images_path)
+            self._length = max(self.num_class_images, self.num_instance_images)
+
+        self.image_transforms = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip(0.5 * hflip),
+                transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
+                transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ]
+        )
+
+    def __len__(self):
+        return self._length
+
+    def __getitem__(self, index):
+        example = {}
+        instance_path, instance_prompt = self.instance_images_path[index % self.num_instance_images]
+        instance_image = Image.open(instance_path)
+        if not instance_image.mode == "RGB":
+            instance_image = instance_image.convert("RGB")
+        
+        example["instance_images"] = self.image_transforms(instance_image)
+        example["instance_prompt_ids"] = self.tokenizer(
+            instance_prompt,
+            padding="max_length" if self.pad_tokens else "do_not_pad",
+            truncation=True,
+            max_length=self.tokenizer.model_max_length,
+            vector_shuffle=self.vector_shuffle
+        ).input_ids
+
+        if self.num_class_images and self.with_prior_preservation:
+            class_path, class_prompt = self.class_images_path[index % self.num_class_images]
+            class_image = Image.open(class_path)
+            if not class_image.mode == "RGB":
+                class_image = class_image.convert("RGB")
+            example["class_images"] = self.image_transforms(class_image)
+            example["class_prompt_ids"] = self.tokenizer(
+                class_prompt,
+                padding="max_length" if self.pad_tokens else "do_not_pad",
+                truncation=True,
+                max_length=self.tokenizer.model_max_length,
+                vector_shuffle=self.vector_shuffle
+            ).input_ids
+
+        return example
+
+
+
+def collate_fn(examples, tokenizer):
+    input_ids = [example["instance_prompt_ids"] for example in examples]
+    pixel_values = [example["instance_images"] for example in examples]
+
+    # Concat class and instance examples for prior preservation.
+    # We do this to avoid doing two forward passes.
+    if "class_images" in examples[0]: # with with_prior_preservation
+        input_ids += [example["class_prompt_ids"] for example in examples]
+        pixel_values += [example["class_images"] for example in examples]
+
+    pixel_values = torch.stack(pixel_values)
+    pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
+
+    padded_tokens = tokenizer.pad(
+        {"input_ids": input_ids},
+        padding=True,
+        return_tensors="pt",
+    )
+
+    batch = {
+        "pixel_values": pixel_values,
+        "input_ids": padded_tokens.input_ids,
+        "attention_mask": padded_tokens.attention_mask,
+    }
+    return batch
+
---
+++ tokenizers.py
@@ -0,0 +1,249 @@
+
+from transformers import CLIPTextModel, CLIPTokenizer
+from transformers import BatchEncoding
+import random
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, DDIMScheduler, DPMSolverMultistepScheduler, LMSDiscreteScheduler, EulerDiscreteScheduler, DDPMScheduler, UNet2DConditionModel, AutoencoderKL
+
+
+
+
+class CLIPTokenizerWithEmbeddings(CLIPTokenizer):
+    '''can read embeddings in automatic1111 format or from torch.save({placeholder_toke : embedding})'''
+    
+    def replace_placeholder_tokens_in_text(self, text, vector_shuffle=False):
+        if hasattr(self, 'token2all_tokens'):
+            for token, all_tokens in self.token2all_tokens.items():
+                if vector_shuffle:
+                    all_tokens = all_tokens[:]
+                    random.shuffle(all_tokens)
+
+                if isinstance(text, list):
+                    text = [t.replace(token, " ".join(all_tokens)) for t in text]
+                else:
+                    text = text.replace(token, " ".join(all_tokens))
+            print(text)
+        return text
+  
+
+    def __call__(self, text, *args, vector_shuffle=False, **kwargs):
+        return super().__call__(self.replace_placeholder_tokens_in_text(text, vector_shuffle=vector_shuffle), *args, **kwargs)
+
+
+    def encode(self, text, *args, vector_shuffle=False, **kwargs):
+        return super().encode(self.replace_placeholder_tokens_in_text(text, vector_shuffle=vector_shuffle), *args, **kwargs)
+
+
+
+    def init_attributes(self):
+        if not hasattr(self, 'token2id'):
+            self.__setattr__('token2id', {})
+            
+        if not hasattr(self, 'token_id2all_token_ids'):
+            self.__setattr__('token_id2all_token_ids', {})
+
+        if not hasattr(self, 'token2all_tokens'):
+            self.__setattr__('token2all_tokens', {})
+
+    def load_embedding(self, token, path, text_encoder, use_orig_token=False):
+        #with torch.no_grad():
+
+            self.init_attributes()  
+            
+            x = torch.load(path)
+            
+            is_automatic1111_embedding = False
+            
+            if 'string_to_param' in x:
+                is_automatic1111_embedding = True
+                
+                
+            if is_automatic1111_embedding:
+                # {
+                #     'string_to_token' : {'*': 265},
+                #     'string_to_param':tensor
+                #     ...
+                #  }
+                
+                # place_holder token will not be used in the embedding but will be replaced with the token argument
+                placeholder_token = list(x['string_to_token'].keys())[0]
+                embedding_matrix = x['string_to_param'][placeholder_token]
+                
+            else:
+                placeholder_token = list(x.keys())[0]
+                embedding_matrix = x[placeholder_token]
+            
+            if use_orig_token:
+                token = placeholder_token
+
+            print(text_encoder.get_input_embeddings().weight.data.dtype)
+                
+            n_vectors = embedding_matrix.shape[0]
+            embedding_size = embedding_matrix.shape[1]
+            
+            tokens_to_add = [token] + [token+str(i) for i in range(n_vectors-1)]
+            num_added_toks = self.add_tokens(tokens_to_add)
+            
+            text_encoder.resize_token_embeddings(self.__len__())
+            
+            token_id = self.convert_tokens_to_ids(token)
+            token_ids = self.convert_tokens_to_ids(tokens_to_add)
+            
+            self.token2id[token] = token_id
+            self.token_id2all_token_ids[token_id] = token_ids
+            self.token2all_tokens[token] = tokens_to_add
+            
+            
+            token_embeds = text_encoder.get_input_embeddings().weight.data
+            
+            token_embeds[token_id:token_id+n_vectors] = embedding_matrix
+            
+
+
+    def add_embedding(self, token, text_encoder, initializer_token=None, n_vectors=4, if_exists='error'):
+        ''' 
+        adds new token to tokenizer with multiple embeddings 
+        @initializer_token: string, eg 'cat'
+        @if_exists: one of ['error', 'append']
+        '''
+
+        self.init_attributes()
+
+        if token in self.token2all_tokens: # token already exists
+            if if_exists=='error':
+                raise ValueError(
+                  f"The tokenizer already contains the token {token}. Please pass a different"
+                  " `placeholder_token` that is not already in the tokenizer."
+                )
+            
+            existing_tokens = self.token2all_tokens[token] # eg ['<glass>', '<glass>0', '<glass>1', '<glass>2']
+            tokens_to_add = [token+str(i+len(existing_tokens)-1) for i in range(n_vectors)]
+            num_added_toks = self.add_tokens(tokens_to_add)
+
+            self.token2all_tokens[token] += tokens_to_add
+
+
+        else:
+
+            tokens_to_add = [token] + [token+str(i) for i in range(n_vectors-1)]
+            num_added_toks = self.add_tokens(tokens_to_add)
+
+            self.token2all_tokens[token] = tokens_to_add
+
+
+        text_encoder.resize_token_embeddings(self.__len__())
+
+        token_ids = self.convert_tokens_to_ids(tokens_to_add)
+
+        token_embeds = text_encoder.get_input_embeddings().weight.data
+
+        if initializer_token:
+
+            init_token_ids = self.encode(initializer_token, add_special_tokens=False)
+            for i, id in enumerate(token_ids):
+                token_embeds[id] = token_embeds[init_token_ids[i * len(init_token_ids)//n_vectors]]
+
+        else:
+            for id in token_ids:
+                token_embeds[id] = torch.randn_like(token_embeds[id])
+
+
+
+    def get_mask(self, accelerator):
+        # Get the mask of the weights that won't change
+        mask = torch.ones(self.__len__()).to(accelerator.device, dtype=torch.bool)
+
+        for token in self.token2all_tokens.keys():
+            token_ids = self.encode(token, add_special_tokens=False)
+            for i in range(len(token_ids)):
+                mask = mask & (torch.arange(self.__len__()) != token_ids[i]).to(accelerator.device)
+        return mask
+
+
+
+
+
+def save_progress(tokenizer, text_encoder, accelerator, save_path):
+
+    for placeholder_token in tokenizer.token2all_tokens.keys():
+        placeholder_token_ids = tokenizer.encode(placeholder_token, add_special_tokens=False)
+
+        learned_embeds = accelerator.unwrap_model(text_encoder).get_input_embeddings().weight[placeholder_token_ids]
+
+        if len(placeholder_token_ids) == 1:
+                learned_embeds = learned_embeds[None]
+        learned_embeds_dict = {placeholder_token: learned_embeds.detach().cpu()}
+        torch.save(learned_embeds_dict, save_path)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    import urllib.request 
+    urllib.request.urlretrieve('https://huggingface.co/spaablauw/FloralMarble/resolve/main/FloralMarble-400.pt', './data/FloralMarble-400.pt')
+
+
+    model_path = 'stabilityai/stable-diffusion-2-1'
+
+    text_encoder = CLIPTextModel.from_pretrained(
+        model_path,
+        subfolder="text_encoder",
+        #revision="fp16"
+    )
+    text_encoder.to(dtype=torch.float16)
+
+
+    tokenizer = CLIPTokenizerWithEmbeddings.from_pretrained(model_path, subfolder="tokenizer")
+
+
+    tokenizer.load_embedding('<flora-marble>', './data/FloralMarble-400.pt', text_encoder)
+
+    # important: version 2.1 768 doesnt work with the scheduler: scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
+    # the 2.1-base does
+    pipe = StableDiffusionPipeline.from_pretrained(model_path, tokenizer=tokenizer, text_encoder=text_encoder, safety_checker=None, torch_dtype=torch.float16).to("cuda")
+
+
+    #pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    #pipe.scheduler = LMSDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    #pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+
+    print(pipe.scheduler.config.prediction_type) # should be 'v_prediction' for 768 version
+
+
+    g_cuda = torch.Generator(device='cuda')
+    seed = 1965858791
+    g_cuda.manual_seed(seed)
+
+
+    prompt = "skulpture of greek warrior in helmet, <flora-marble>, red colors, fire"
+    negative_prompt = "" 
+
+    num_samples = 1
+    guidance_scale = 7
+    num_inference_steps = 50
+    height = 768 #768 # 512
+    width = 768  #768 # 512
+
+
+    # DONT use torch.autocast("cuda") here to fix black blank images
+    with torch.inference_mode():
+        images = pipe(
+            prompt,
+            height=height,
+            width=width,
+            negative_prompt=negative_prompt,
+            num_images_per_prompt=num_samples,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            generator=g_cuda
+        ).images
+
+    for i, img in enumerate(images):
+        img.save(f'./data/{i}.png')
