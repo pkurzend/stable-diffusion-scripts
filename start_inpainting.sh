fabda0d9d06e8f41857d55f0fb8aca93121a97ba
Friedi <friedemann.schestag@outlook.com>
2023-01-21 00:31:34 +0100

    included inpainting script, start command

---
+++ start_inpainting.sh
@@ -0,0 +1,26 @@
+venv/bin/accelerate launch train_inpainting_dreambooth.py \
+  --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
+  --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
+  --output_dir="stable_diffusion_weights/inpainting-test" \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --seed=3434554 \
+  --resolution=768 \
+  --train_batch_size=2 \
+  --train_text_encoder \
+  --mixed_precision="fp16" \
+  --gradient_checkpointing \
+  --use_8bit_adam \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=2e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=3 \
+  --sample_batch_size=4 \
+  --max_train_steps=10 \
+  --center_crop \
+  --save_interval=8 \
+  --save_min_steps=6000 \
+  --save_infer_steps=35 \
+  --concepts_list="concepts_list.json" \
+  --not_cache_latents \
+  --hflip
